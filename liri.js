22043ee03654d49b2da233f6643b3f80b4370245
SamanthaAO <samanthaaorcutt@gmail.com>
2019-05-31 20:52:06 -0700

    created OMDB functionality

--- liri.js
+++ liri.js
@@ -33,9 +33,9 @@ switch (action) {
     spotifyThisSong();
     break;
 
-    // case "movie-this":
-    // movieThis();
-    // break;
+    case "movie-this":
+    movieThis();
+    break;
 
     // case "do-what-it-says":
     // doWhatItSays();
@@ -45,15 +45,16 @@ switch (action) {
 //gana use a switch here!!!
 // concert-this
 function concertThis(){
-   var queryURL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
+   var concertQueryURL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
 
-   axios.get(queryURL).then(
+   axios.get(concertQueryURL).then(
   function(response) {
 
 response.data.forEach(function(element, num){
     console.log(num+1 +" : " +element.venue.name);
     console.log(element.venue.city +", "+ element.venue.country);
     console.log(moment(element.datetime).format("MM/DD/YYYY"));
+    console.log();
 
 
 })
@@ -84,7 +85,7 @@ response.data.forEach(function(element, num){
 
 // // spotify-this-song
 function spotifyThisSong(){
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    spotify.search({ type: 'track', query: input }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
@@ -99,43 +100,57 @@ function spotifyThisSong(){
       console.log(artists.join(", "));
       console.log(element.album.name); 
       console.log(element.preview_url);
+      console.log();
 
     })
       
-      //console.log(data.tracks.items); 
       });
 
 }
 
 
-// // movie-this
-// function movieThis(){
-// Then run a request with axios to the OMDB API with the movie specified
-// axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-//   function(response) {
-//     console.log("The movie's rating is: " + response.data.imdbRating);
-//   })
-//   .catch(function(error) {
-//     if (error.response) {
-//       // The request was made and the server responded with a status code
-//       // that falls out of the range of 2xx
-//       console.log("---------------Data---------------");
-//       console.log(error.response.data);
-//       console.log("---------------Status---------------");
-//       console.log(error.response.status);
-//       console.log("---------------Status---------------");
-//       console.log(error.response.headers);
-//     } else if (error.request) {
-//       // The request was made but no response was received
-//       // `error.request` is an object that comes back with details pertaining to the error that occurred.
-//       console.log(error.request);
-//     } else {
-//       // Something happened in setting up the request that triggered an Error
-//       console.log("Error", error.message);
-//     }
-//     console.log(error.config);
-//   });
-// }
+// movie-this
+function movieThis(){
+
+  var movieQueryURL = "http://www.omdbapi.com/?t=" + input +"&y=&plot=short&apikey=trilogy"
+
+//Then run a request with axios to the OMDB API with the movie specified
+axios.get(movieQueryURL).then(
+  function(response) {
+
+    console.log(response.data.Title);
+    console.log(response.data.Year);
+    console.log("The movie's IMDB rating is: " + response.data.imdbRating);
+    console.log(response.data.Ratings[1].Source +" "+response.data.Ratings[1].Value);
+    console.log(response.data.Language);
+    console.log(response.data.Plot);
+    console.log(response.data.Actors);
+
+    
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+}
+
+
 
 // // do-what-it-says
 // function doWhatItSays(){
